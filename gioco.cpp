7ce13c9ebe62f36c5478a4b642ab2597400cc53d
GabryButti <91748097+GabryButti@users.noreply.github.com>
2021-10-01 19:25:57 +0200

    Update gioco.cpp

--- gioco.cpp
+++ gioco.cpp
@@ -4,11 +4,11 @@
 #include <windows.h>
 #include <conio.h>
 #include <iostream>
-using namespace std;
-#define W_CONSOLE       100
-#define H_CONSOLE       15
-#define CHAR_NAVICELLA  'O'
 
+using namespace std;
+#define W_CONSOLE       83
+#define H_CONSOLE       20
+#define CHAR_NAVICELLA  L'\028'
 HANDLE gHndConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 void show_console_cursor(bool show) {
@@ -37,53 +37,68 @@ void init_console(SHORT wConsole, SHORT hConsole) {
     set_console_size(wConsole, hConsole);
     SHORT a = 0;
     for (int i = 0; i < wConsole; i++) {
-        gotoxy(a, 0); putchar('Q');
-        gotoxy(a, hConsole); putchar('Q');
+        gotoxy(a, 0); putchar('_');
+        gotoxy(a, hConsole); putchar('_');
         a=a+1;
     }
-    set_console_color(FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+    set_console_color(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     show_console_cursor(false);
 }
-
+/*
 void show_char_at(SHORT x, SHORT y, char c) {
-    gotoxy(x, y);     putchar (c);
+    gotoxy(x, y);     putchar(c);
     gotoxy(x - 1, y); putchar(' ');
     gotoxy(x + 1, y); putchar(' ');
     gotoxy(x, y - 1); putchar(' ');
     gotoxy(x, y + 1); putchar(' ');
 }
-
+*/
+void show_char_at(SHORT x, SHORT y, wchar_t c, char tasto) {
+    if (tasto == 77) {
+        gotoxy(x - 1, y); putchar(' ');
+    }
+    else if(tasto == 75){
+        gotoxy(x+1, y); putchar(' ');
+    }
+    else if (tasto == 80) {
+        gotoxy(x, y - 1); putchar(' ');
+    }
+    else if (tasto == 72) {
+        gotoxy(x, y + 1); putchar(' ');
+    }
+    gotoxy(x, y);     putwchar(c);
+}
 int main() {
     SHORT x = W_CONSOLE / 2;
     SHORT y = H_CONSOLE - 1;
 
     init_console(W_CONSOLE, H_CONSOLE);
-    show_char_at(x, y, CHAR_NAVICELLA);
+    show_char_at(x, y, CHAR_NAVICELLA, 0);
 
     while (1 != 0) {
         show_console_cursor(false);
         char tasto = _getch();
-    if (tasto == 75)
-            {
-                x = x - 1 >= 0 ? x - 1 : x;                         //sinistra
-            }
-            else if (tasto == 77){
+        if (tasto == 75)
+        {
+            x = x - 1 >= 0 ? x - 1 : x;                         //sinistra
+        }
+        else if (tasto == 77) {
 
-                x = x + 1 < W_CONSOLE ? x + 1 : x;                  //destra
+            x = x + 1 < W_CONSOLE ? x + 1 : x;                  //destra
 
-            }
-            else if (tasto == 72){
+        }
+        else if (tasto == 72) {
 
-                y = y - 1 >= 1 ? y - 1 : y;                         //giù
+            y = y - 1 >= 1 ? y - 1 : y;                         //su
 
-            }
-            else if (tasto == 80){
+        }
+        else if (tasto == 80) {
 
-                y = y + 1 < H_CONSOLE ? y + 1 : y;                  //su
+            y = y + 1 < H_CONSOLE ? y + 1 : y;                  //giù
 
-            }
+        }
 
-        show_char_at(x, y, CHAR_NAVICELLA);
+        show_char_at(x, y, CHAR_NAVICELLA, tasto);
     }
 }
 
