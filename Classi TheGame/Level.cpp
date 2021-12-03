f3c8c41eadc9cea979a2dbba4fe99de20f3ae0aa
BettoScuderi <34723016+BettoScuderi@users.noreply.github.com>
2021-12-03 19:05:46 +0100

    Update Level.cpp

--- Classi TheGame/Level.cpp
+++ Classi TheGame/Level.cpp
@@ -1,1 +1,23 @@
 #include "Level.h"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include<windows.h>
+#include<conio.h>
+
+
+using namespace std;
+
+#define W_CONSOLE 83
+#define H_CONSOLE 20
+#define CHAR_NAVICELLA L'\028'
+
+HANDLE gHndConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void platform(int x, int y, int z) {
+	SHORT a = x;
+	for (int i = 0; i < z; i++) {
+		gotoxy(a, y); putchar('I');
+		a = a + 1;
+	}
+}
