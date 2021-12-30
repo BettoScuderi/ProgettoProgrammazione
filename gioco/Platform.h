08a06cc25a910dddc1587dd43e818b55a1b912a1
GabryButti <91748097+GabryButti@users.noreply.github.com>
2021-12-30 19:54:40 +0100

    Update Platform.h

--- gioco/Platform.h
+++ gioco/Platform.h
@@ -3,13 +3,11 @@ class Platform
 {
 private:
 	int _length;
-	int _width;
 	int _height;
 	bool _drawn;
 public:
-	Platform(int length, int width, int height) {
+	Platform(int length, int height) {
 		_length = length;
-		_width = width;
 		_height = height;
 		_drawn = false;
 	}
